a1cdfc465e2d1172a2cc84bc0c15b74ae35dde68
Jose Jereza <92663129+jvjereza@users.noreply.github.com>
2023-03-03 10:04:39 +0800

    updated again

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,10 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App.js';
+import { render } from '@testing-library/react';
+
+test('renders a heading with the correct text', () => {
+  const { getByRole } = render(<App />);
+  const heading = getByRole('heading', { name: /my todo list/i });
+  expect(heading).toHaveTextContent('My Todo List');
+});
+
